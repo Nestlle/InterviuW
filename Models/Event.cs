5ef8f24b868968e487e730c8cdc40c7ae406de4b
VladTC <tomavlad90@gmail.com>
2021-06-20 22:56:12 +0300

    Added required to some fields
    Changes according to design specification

--- Models/Event.cs
+++ Models/Event.cs
@@ -1,16 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace InterviuW.Models
 {
     public class Event
     {
+        [Required]
         public string OrganiserName { get; set; }
         public int EventId { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
+        [Required]
         public string City { get; set; }
         private DateTime eventDate;
+        [Required]
         public DateTime? EventDate
         {
             get { return eventDate; }
